c8613f9dc9656ba873f7bda0ef9aee62199a6553
Ivan Smirnov <ismirnov@live.com>
2022-01-02 20:40:28 +0100

    Refactor

--- Implementation/AdventPackage/Tests/AdventOfCodeTests/AdventOfCodeTests.swift
+++ Implementation/AdventPackage/Tests/AdventOfCodeTests/AdventOfCodeTests.swift
@@ -1,48 +1,6 @@
 import XCTest
 @testable import AdventOfCode
 
-struct Submarine {
-    let sonar: Sonar
-    //For now let's perform sonar sweep only when depth changes,
-    //not automatically in time intervals
-    var depth: Depth {
-        didSet {
-            performSonarSweep()
-        }
-    }
-    var didPerformSonarSweep: (_ depths: [Depth]) -> Void
-    
-    func performSonarSweep() {
-        let depths = sonar.performSweep()
-        didPerformSonarSweep(depths)
-    }
-}
-
-struct Sonar {
-    var performSweep: () -> [Depth]
-}
-
-struct Depth: Equatable {
-    let value: Int
-}
-
-struct DepthAnalyzer {
-    static func process(input: [Depth]) throws -> Int {
-        guard !input.isEmpty else {
-            throw "Empty Input Error"
-        }
-        var count = 0
-        for i in 1..<input.count {
-            if input[i].value > input[i-1].value {
-                count += 1
-            }
-        }
-        return count
-    }
-}
-
-extension String: Error {}
-
 //MARK: - Tests-only code
 extension Sonar {
     static func withMockDepths(mock: [Depth]) -> Self {

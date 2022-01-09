c858e69264dd39ba03390b05f2c4c921767fd794
Ivan Smirnov <ismirnov@live.com>
2022-01-09 18:41:07 +0100

    Add test navigator_returnsNewPosition_fromInitialPosition_withMovementRoute

--- Implementation/AdventPackage/Tests/AdventOfCodeTests/NavigatorTests.swift
+++ Implementation/AdventPackage/Tests/AdventOfCodeTests/NavigatorTests.swift
@@ -4,7 +4,7 @@ import XCTest
 // Horizontal distance from the origin.
 // For Submarine it is a distance from the ship
 struct HorizontalDistance: Equatable {
-    let amount: Int
+    let value: Int
 }
 
 struct Position: Equatable {
@@ -12,14 +12,29 @@ struct Position: Equatable {
     let distance: HorizontalDistance
     
     static var zero: Self {
-        return .init(depth: .init(value: 0), distance: .init(amount: 0))
+        return .init(depth: .init(value: 0), distance: .init(value: 0))
     }
 }
 
 struct Navigator {
-    
     func position(from initial: Position, with route: MovementRoute) -> Position {
-        return initial
+        guard !route.isEmpty else {
+            return initial
+        }
+        
+        var depthValue = initial.depth.value
+        var distanceValue = initial.distance.value
+        route.commands.forEach { command in
+            switch command.direction {
+            case .forward:
+                distanceValue += command.amount
+            case .down:
+                depthValue += command.amount
+            case .up:
+                depthValue -= command.amount
+            }
+        }
+        return .init(depth: .init(value: depthValue), distance: .init(value: distanceValue))
     }
 }
 
@@ -59,4 +74,21 @@ final class NavigatorTests: XCTestCase {
         XCTAssertEqual(start, end)
     }
     
+    func test_navigator_returnsNewPosition_fromInitialPosition_withMovementRoute() {
+        let sut = Navigator()
+        let route = MovementRoute(commands: [
+            .init(direction: .forward, amount: 5),
+            .init(direction: .down, amount: 5),
+            .init(direction: .forward, amount: 8),
+            .init(direction: .up, amount: 3),
+            .init(direction: .down, amount: 8),
+            .init(direction: .forward, amount: 2)
+        ])
+        
+        let end = sut.position(from: Position.zero, with: route)
+        let expectedPosition = Position.init(depth: .init(value: 10), distance: .init(value: 15))
+        
+        XCTAssertEqual(end.depth, expectedPosition.depth)
+        XCTAssertEqual(end.distance, expectedPosition.distance)
+    }
 }
